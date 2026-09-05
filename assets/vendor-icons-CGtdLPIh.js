89915effffd5428a1b906e20a9419306eb0d16da
armazemguarabira-del <armazemguarabira-del@users.noreply.github.com>
2026-09-05 13:10:11 +0000

    deploy: f61ef27084baf86c2a9d50efcc867dde3e1acce9

--- assets/vendor-icons-CGtdLPIh.js
+++ assets/vendor-icons-CGtdLPIh.js
@@ -1,4 +1,4 @@
-import{r as d}from"./vendor-motion-R-v-Ilmk.js";/**
+import{r as d}from"./vendor-react-Y_kS7UQV.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
